7b2378f60eac4100f102dde26935cf878c958e9c
Bruno Guedes <brunoguedes@brunos-macbook-pro.home>
2017-11-08 15:54:12 +0100

    first commit

---
+++ src/main/java/com/couchbase/day/config/Database.java
@@ -0,0 +1,41 @@
+package com.couchbase.day.config;
+
+import com.couchbase.client.java.Bucket;
+import com.couchbase.client.java.Cluster;
+import com.couchbase.client.java.CouchbaseCluster;
+import com.couchbase.client.java.env.CouchbaseEnvironment;
+import com.couchbase.client.java.env.DefaultCouchbaseEnvironment;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class Database {
+
+	@Value("${storage.host}")
+	private String host;
+
+	@Value("${storage.bucket}")
+	private String bucket;
+
+	@Value("${storage.username}")
+	private String username;
+
+	@Value("${storage.password}")
+	private String password;
+
+	public @Bean Cluster couchbaseCluster() {
+
+		//CouchbaseEnvironment env = DefaultCouchbaseEnvironment.builder().queryEndpoints(1).build();
+
+		//CouchbaseCluster cluster = CouchbaseCluster.create(env, host);
+		CouchbaseCluster cluster = CouchbaseCluster.create(host);
+		cluster.authenticate(username, password);
+		return cluster;
+	}
+
+	public @Bean Bucket loginBucket() {
+		return couchbaseCluster().openBucket(bucket);
+	}
+}
